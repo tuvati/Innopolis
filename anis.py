46d2431be8602ca71d907695218fe24917aa8f21
tuvati <tuvati@mail.ru>
2023-08-24 05:29:09 +0300

    test

---
+++ anis.py
@@ -0,0 +1,65 @@
+# print ("Привет Оля")
+# 136 // 7
+# print (5)
+# input ("введите свой возраст")
+# print("Результат: ", 7, 15, 25, sep="|", end="%")
+# print ("задолбало", 5**2 )
+# print ( 5**3)
+# print (5 // 3)
+number = 5
+# print ("Результат:", number)
+# num1 = int(input ("введите первое число: "))
+# num2 = int(input ("введите второе число: "))
+# print ("result:", num1+num2)
+# print ("result:", num1-num2)
+# print ("result:", num1/num2)
+# print ("result:", num1*num2)
+# 136 // 7
+# if 5 == 5:
+#     print ('yes')
+#     print('!!')
+
+# user_data = int(input("Введите число: "))
+
+# isHappy = True
+# == - равно, != - неравно, >=,  <=,
+# if isHappy or user_data == 6:
+#     print("User is happy")
+# elif user_data == 5:
+#     print("Number is 5")
+# elif user_data == 7:
+#     print("Number is 7")
+# else:
+#     print("User is unhappy")
+
+# if user_data != 5:
+#     print("Мы на месте")
+#     if user_data > 6:
+#         print("Number is bigger than 5")
+
+# for i in range(6):
+#      print(i)
+# for i in range(1,6,2):
+#      print(i)
+word = "Привет Оля"
+# for i in word:
+#      print(i*3)
+
+# i=5
+# while i < 15:
+#      print(i)
+#      i += 2
+# file = open('data/test.txt', 'w')
+# file.write('hello')
+# file.close()
+# file = open('data/test.txt','r')
+# print (file.read(2))
+# file.close()
+# import sys
+# print(sys.path)
+# __init__
+import webbrowser
+def open_url(url):
+    webbrowser.open(url)
+open_url('https://itproger.com')
+git version
